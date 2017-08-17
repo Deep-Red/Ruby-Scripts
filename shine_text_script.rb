b50d1395b3cecbb3a2bb88601253c1731af5710b
Deep Red <0k5786@gmail.com>
2017-08-17 10:56:27 -0400

    Fixed bug in shine_text_script that showed up on AddressComponent (field_name based on old bindon-ngModel attribute, not name attribute).

--- shine_text_script.rb
+++ shine_text_script.rb
@@ -14,9 +14,11 @@ contents = File.read(shinefile)
 contents.scan(/<div class="form-group"(.*?)type="text"(.*?)<\/div>/m) do |m|
   m = m.to_s
   bo    = contents.match(/(?<=ngModel=\")(.*)(?=\.)/).to_s
-  fn    = contents.match(/(?<= name=\")(.*)(?=\" )/).to_s
-  fn    = fn.split("-")*'_'
+  fn    = contents.match(/(?<=ngModel=\")(.*)(?=\")/).to_s.split(".")[1]
   l     = fn.split("_").map(&:capitalize)*' '
+  if bo == "customer"
+    fn    = fn.split("-")*'_'
+  end
   contents = contents.sub(/<div class="form-group"(.*?)type="text"(.*?)<\/div>/m, "<shine-text-field bind-object=\"#{bo}\" field_name=\"#{fn}\" label=\"#{l}\"></shine-text-field>")
   # Gives some idea of progress by printing out filesize
   puts contents.size
