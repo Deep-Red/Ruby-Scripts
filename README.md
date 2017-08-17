8f81d4f549a27a17c2176fd2707a098427cd59e1
Deep Red <0k5786@gmail.com>
2017-08-17 10:16:05 -0400

    Script to assist with Angular integration in Shine App (RAPB-Shine).

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ruby-Scripts
+
+** shine_text_script **
+Script to substitute markup in html templates for Angular integration
---
+++ shine_text_script.rb
@@ -0,0 +1,27 @@
+# End the program if not supplied with exactly one file to work on
+if ARGV.length != 1
+  abort("Usage: shine_text_script.rb filename.ext")
+end
+
+# Extract filename and path
+shinefile = ARGV[0]
+
+# Open file
+#afile = File.open(shinefile, "r+")
+# Read file in as string
+contents = File.read(shinefile)
+
+contents.scan(/<div class="form-group"(.*?)type="text"(.*?)<\/div>/m) do |m|
+  m = m.to_s
+  bo    = contents.match(/(?<=ngModel=\")(.*)(?=\.)/).to_s
+  fn    = contents.match(/(?<= name=\")(.*)(?=\" )/).to_s
+  fn    = fn.split("-")*'_'
+  l     = fn.split("_").map(&:capitalize)*' '
+  contents = contents.sub(/<div class="form-group"(.*?)type="text"(.*?)<\/div>/m, "<shine-text-field bind-object=\"#{bo}\" field_name=\"#{fn}\" label=\"#{l}\"></shine-text-field>")
+  # Gives some idea of progress by printing out filesize
+  puts contents.size
+end
+
+File.open(shinefile, "w"){|file| file.puts contents}
+# Close file
+#afile.close
