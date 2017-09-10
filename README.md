59c9c33ead82ae89094e9405d495cc843e31e38e
Deep Red <0k5786@gmail.com>
2017-09-10 08:29:15 -0400

    mtt_downloader script. Also added folder for scripts to share downloads to reduce clutter.

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Ruby-Scripts
 
-** shine_text_script **
-*Script to substitute markup in html templates for Angular integration
-*Created to automate markup substitution for Copeland's RAPB
-*Development discontinued in favor of using sed (Still works for original purpose, just will not be extended)
+** shine_text_script **  
+*Script to substitute markup in html templates for Angular integration  
+*Created to automate markup substitution for Copeland's RAPB  
+*Development discontinued in favor of using sed (Still works for original purpose, just will not be extended)  
 
 
-** image_downloader **
-*Script to download images from a webpage
-*Created to automate downloading of images for Depot app in AWDR5
-*Currently only pulls images from links in li elements
+** image_downloader **  
+*Script to download images from a webpage  
+*Created to automate downloading of images for Depot app in AWDR5  
+*Currently only pulls images from links in li elements  
+
+** mtt_downloader **  
+*Downloads theme/title/artist from reddit.com/r/musicthemetime  
+*Fairly restricted filter for post titles, trying to keep them as clean as possible for now
---
+++ mtt_downloader.rb
@@ -0,0 +1,46 @@
+require 'open-uri'
+require 'json'
+
+SUBREDDIT = "musicthemetime"
+SORT = "new"
+TIME = "week"
+
+@count = 0
+@after = nil
+
+if SORT == "top"
+  @url = "http://www.reddit.com/r/#{SUBREDDIT}/#{SORT}/.json?sort=#{SORT}&t=#{TIME}"
+  @output = File.open("downloads/#{SUBREDDIT}-#{SORT}-#{TIME}.csv", "w")
+else
+  @url = "http://www.reddit.com/r/#{SUBREDDIT}/#{SORT}/.json?sort=#{SORT}"
+  @output = File.open("downloads/#{SUBREDDIT}-#{SORT}.csv", "w")
+end
+
+
+39.times do
+  page = JSON.load(open(@url, {'User-Agent' => 'qazwsxedcujmik'}))
+
+  page["data"]["children"].each do |child|
+    if child["data"]["link_flair_text"] && child["data"]["title"]
+      post_flair = child["data"]["link_flair_text"]
+      post_title = child["data"]["title"]
+
+      if post_title.match(/^[A-Za-z\s]+\s-\s[A-Za-z\s]+$/) && post_flair.match(/^[A-Za-z\s]+$/)
+        separated = post_title.split(" - ")
+        title = separated[1]
+        artist = separated[0]
+        @output.puts post_flair + "," + title + "," + artist
+      end
+
+    end
+  end
+  @count += 25
+  @after = page["data"]["after"]
+
+  if SORT == "top"
+    @url = "http://www.reddit.com/r/#{SUBREDDIT}/#{SORT}/.json?sort=#{SORT}&t=#{TIME}&count=#{@count}&after=#{@after}"
+  else
+    @url = "http://www.reddit.com/r/#{SUBREDDIT}/#{SORT}/.json?sort=#{SORT}&count=#{@count}&after=#{@after}"
+  end
+
+end
